f17c9e8684c616851ccb184848718b7ae392dff0
Marco Lam <lamma3@oocl.com>
2020-04-20 17:13:03 +0800

    remove useless singleton code in EmployeeMapper

--- src/main/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapper.java
@@ -4,12 +4,9 @@ import com.thoughtworks.springbootemployee.model.db.Employee;
 import com.thoughtworks.springbootemployee.model.request.EmployeeRequest;
 import com.thoughtworks.springbootemployee.model.response.EmployeeResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
-    EmployeeMapper INSTANCE = Mappers.getMapper(EmployeeMapper.class);
-
     Employee employeeRequestToEmployee(EmployeeRequest employeeRequest);
     EmployeeResponse employeeToEmployeeResponse(Employee employee);
 }
